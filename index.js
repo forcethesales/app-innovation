8c950671ab82aed95c97ddf261397383dab28f7d
forcethesales <jessierymph@gmail.com>
2017-09-19 11:21:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -73,15 +86,4 @@ app.get('/children', function (req, res, next) {
 //////
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
 });
